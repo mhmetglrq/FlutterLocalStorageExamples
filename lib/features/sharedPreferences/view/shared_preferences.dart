4c51633f8f7be7272ab8bb97cf6979eee24eb0c2
Mehmet Güler <mhmetglr.q@gmail.com>
2024-01-28 15:55:22 +0300

    MVC created!

---
+++ lib/features/sharedPreferences/view/shared_preferences.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SharedPreferencesPage extends StatefulWidget {
+  const SharedPreferencesPage({super.key});
+
+  @override
+  State<SharedPreferencesPage> createState() => _SharedPreferencesPageState();
+}
+
+class _SharedPreferencesPageState extends State<SharedPreferencesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Center(
+            child: Column(
+              children: [
+                Text('SharedPreferences'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
