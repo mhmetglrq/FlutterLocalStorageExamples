3e28ad450225d2813a79678c57058f5224665194
Mehmet Güler <mhmetglr.q@gmail.com>
2024-01-28 16:46:34 +0300

    Examples added!

--- lib/features/sharedPreferences/repository/shared_preferences_repository.dart
+++ lib/features/sharedPreferences/repository/shared_preferences_repository.dart
@@ -2,92 +2,131 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final sharedPreferencesRepositoryProvider = Provider(
-  (ref) {
-    SharedPreferences? prefs;
-    SharedPreferences.getInstance().then((value) => prefs = value);
-    return SharedPreferencesRepository(
-      prefs: prefs,
-    );
+  (ref) => SharedPreferencesRepository(),
+);
+
+final getStringProvider = FutureProvider.autoDispose.family<String?, String>(
+  (ref, key) async {
+    final sharedPreferencesRepository =
+        ref.watch(sharedPreferencesRepositoryProvider);
+
+    return sharedPreferencesRepository.getString(key);
   },
 );
 
 class SharedPreferencesRepository {
-  SharedPreferences? prefs;
-  SharedPreferencesRepository({required this.prefs});
+  SharedPreferencesRepository();
 
   Future<void> saveString(String key, String value) async {
-    await prefs?.setString(key, value);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(key, value);
   }
 
   Future<String?> getString(String key) async {
-    return prefs?.getString(key);
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString(key);
   }
 
   Future<void> saveBool(String key, bool value) async {
-    await prefs?.setBool(key, value);
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setBool(key, value);
   }
 
   Future<bool?> getBool(String key) async {
-    return prefs?.getBool(key);
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getBool(key);
   }
 
   Future<void> saveInt(String key, int value) async {
-    await prefs?.setInt(key, value);
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setInt(key, value);
   }
 
   Future<int?> getInt(String key) async {
-    return prefs?.getInt(key);
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getInt(key);
   }
 
   Future<void> saveDouble(String key, double value) async {
-    await prefs?.setDouble(key, value);
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setDouble(key, value);
   }
 
   Future<double?> getDouble(String key) async {
-    return prefs?.getDouble(key);
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getDouble(key);
   }
 
   Future<void> saveStringList(String key, List<String> value) async {
-    await prefs?.setStringList(key, value);
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setStringList(key, value);
   }
 
   Future<List<String>?> getStringList(String key) async {
-    return prefs?.getStringList(key);
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getStringList(key);
   }
 
   Future<void> remove(String key) async {
-    await prefs?.remove(key);
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.remove(key);
   }
 
   Future<void> clear() async {
-    await prefs?.clear();
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.clear();
   }
 
   Future<bool> containsKey(String key) async {
-    return prefs!.containsKey(key);
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.containsKey(key);
   }
 
   Future<Set<String>> getKeys() async {
-    return prefs!.getKeys();
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getKeys();
   }
 
   Future<void> reload() async {
-    await prefs?.reload();
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.reload();
   }
 
   Future<bool> isInitialized() async {
-    return prefs!.getKeys().isNotEmpty;
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getKeys().isNotEmpty;
   }
 
   Future<bool> isNotInitialized() async {
-    return prefs!.getKeys().isEmpty;
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getKeys().isEmpty;
   }
 
   Future<bool> isInitializedKey(String key) async {
-    return prefs!.containsKey(key);
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.containsKey(key);
   }
 
   Future<bool> isNotInitializedKey(String key) async {
-    return !prefs!.containsKey(key);
+    final prefs = await SharedPreferences.getInstance();
+
+    return !prefs.containsKey(key);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_storage/features/sharedPreferences/view/shared_preferences.dart';
 
 Future<void> main() async {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
