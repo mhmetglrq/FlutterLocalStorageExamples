5cc43a4e77887e67ddde40547839cf99ab878fa7
Mehmet Güler <mhmetglr.q@gmail.com>
2024-01-28 19:01:09 +0300

    MVC created!

---
+++ lib/features/hive/view/hive_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HivePage extends StatefulWidget {
+  const HivePage({super.key});
+
+  @override
+  State<HivePage> createState() => _HivePageState();
+}
+
+class _HivePageState extends State<HivePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
